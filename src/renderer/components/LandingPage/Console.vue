e78724ae7138aad5af64933db505362ee9575fca
nutkaewnak <nut-boss@hotmail.com>
2017-07-04 23:18:20 +0700

    implemented mouse position

--- src/renderer/components/LandingPage/Console.vue
+++ src/renderer/components/LandingPage/Console.vue
@@ -1,30 +1,59 @@
 <template>
   <div>
-    <div id="diagramContainer">
-        <vue-draggable-resizable :minw="100" :minh="100" :w="100" :h="100" :x="0" :y="0" :parent="true" id="item_left" class="item">item_left</vue-draggable-resizable>
-        <vue-draggable-resizable :minw="100" :minh="100" :w="100" :h="100" :x="100" :y="0" :parent="true" id="item_right" class="item">item_right</vue-draggable-resizable>
+    <div
+    @mouseup="handleClick"
+    @touchend="handleClick"
+    id="diagramContainer" 
+    v-on:dblclick="jsPlumb.newNode(cursorPos.top, cursorPos.left)">
+      <vue-draggable-resizable :minw="100" :minh="100" :w="100" :h="100" :x="0" :y="0" :parent="true" id="item_left" class="item">
+      item_left</vue-draggable-resizable>
+      <vue-draggable-resizable :minw="100" :minh="100" :w="100" :h="100" :x="100" :y="0" :parent="true" id="item_right" class="item">
+      item_right</vue-draggable-resizable>
     </div>
   </div>
 </template>
 
 <script>
-  var jsPlumb = require('../../../../node_modules/jsplumb/dist/js/jsplumb.js').jsPlumb
+var jsPlumb = require('../../../../node_modules/jsplumb/dist/js/jsplumb.js').jsPlumb
 
-  jsPlumb.ready(function () {
-    jsPlumb.connect({
-      source: 'item_left',
-      target: 'item_right',
-      endpoint: 'Rectangle'
-    })
-
-    jsPlumb.draggable('item_left', {containment: 'parent'})
-    jsPlumb.draggable('item_right', {containment: 'parent'})
+jsPlumb.ready(function () {
+  jsPlumb.connect({
+    source: 'item_left',
+    target: 'item_right',
+    endpoint: ['Dot', {radius: 2}],
+    HoverPaintStyle: {stroke: '#1e8151', strokeWidth: 2},
+    ConnectionOverlays: [
+      [ 'Arrow', {
+        location: 1,
+        id: 'arrow',
+        length: 14,
+        foldback: 0.8
+      } ],
+      ['Label', { label: 'FOO', id: 'label', cssClass: 'aLabel' }]
+    ],
+    Container: 'canvas'
   })
-  export default {
-    props: ['robot', 'robots'],
-    methods: {
+
+  jsPlumb.draggable('item_left', {containment: 'parent'})
+  jsPlumb.draggable('item_right', {containment: 'parent'})
+})
+
+export default {
+  props: ['robot', 'robots'],
+  data () {
+    return {
+      cursorPos: {}
+    }
+  },
+  methods: {
+    handleClick (e) {
+      this.cursorPos = {
+        top: e.clientY,
+        left: e.clientX
+      }
     }
   }
+}
 </script>
 
 <style scoped>
